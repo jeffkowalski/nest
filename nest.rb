4e02aa8698afa2656bdba795f61cf8d53f3df312
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-11-24 20:34:50 -0800

    correct some issues reported by rubocop

--- nest.rb
+++ nest.rb
@@ -8,19 +8,17 @@ require 'rest-client'
 require 'json'
 require 'influxdb'
 
-
 LOGFILE = File.join(Dir.home, '.log', 'nest.log')
 CREDENTIALS_PATH = File.join(Dir.home, '.credentials', 'nest.yaml')
 
-
 class Nest < Thor
-  no_commands {
+  no_commands do
     def redirect_output
       unless LOGFILE == 'STDOUT'
         logfile = File.expand_path(LOGFILE)
-        FileUtils.mkdir_p(File.dirname(logfile), :mode => 0755)
+        FileUtils.mkdir_p(File.dirname(logfile), mode: 0o755)
         FileUtils.touch logfile
-        File.chmod 0644, logfile
+        File.chmod 0o644, logfile
         $stdout.reopen logfile, 'a'
       end
       $stderr.reopen $stdout
@@ -30,32 +28,33 @@ class Nest < Thor
     def setup_logger
       redirect_output if options[:log]
 
-      $logger = Logger.new STDOUT
-      $logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
-      $logger.info 'starting'
+      @logger = Logger.new STDOUT
+      @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
+      @logger.info 'starting'
     end
-  }
-
-  class_option :log,     :type => :boolean, :default => true, :desc => "log output to #{LOGFILE}"
-  class_option :verbose, :type => :boolean, :aliases => "-v", :desc => "increase verbosity"
-
-
-  desc "authorize", "[re]authorize the application"
-  def authorize
-    credentials = YAML.load_file CREDENTIALS_PATH
-    state = Time::now.to_i
-    puts "Go to this URL https://home.nest.com/login/oauth2?client_id=#{credentials[:client_id]}&state=#{state}"
-    puts "Copy PIN and paste it here:"
-    pin = $stdin.gets.chomp
-    response = RestClient.post("https://api.home.nest.com/oauth2/access_token",
-                               {code: pin, client_id: credentials[:client_id], client_secret: credentials[:client_secret], grant_type: 'authorization_code'})
-    token = JSON.parse(response)
-    credentials[:access_token] = token['access_token']
-    File.open(CREDENTIALS_PATH, "w") { |file| file.write(credentials.to_yaml) }
   end
 
+  class_option :log,     type: :boolean, default: true, desc: "log output to #{LOGFILE}"
+  class_option :verbose, type: :boolean, aliases: '-v', desc: 'increase verbosity'
 
-  desc "record-status", "record the current usage data to database"
+  desc 'authorize', '[re]authorize the application'
+  def authorize
+    credentials = YAML.load_file CREDENTIALS_PATH
+    state = Time.now.to_i
+    puts "Go to this URL https://home.nest.com/login/oauth2?client_id=#{credentials[:client_id]}&state=#{state}"
+    puts 'Copy PIN and paste it here:'
+    pin = $stdin.gets.chomp
+    response = RestClient.post('https://api.home.nest.com/oauth2/access_token',
+                               code: pin,
+                               client_id: credentials[:client_id],
+                               client_secret: credentials[:client_secret],
+                               grant_type: 'authorization_code')
+    token = JSON.parse(response)
+    credentials[:access_token] = token['access_token']
+    File.open(CREDENTIALS_PATH, 'w') { |file| file.write(credentials.to_yaml) }
+  end
+
+  desc 'record-status', 'record the current usage data to database'
   def record_status
     setup_logger
 
@@ -63,67 +62,68 @@ class Nest < Thor
 
     response = RestClient::Request.execute(
       method: 'get',
-      url: "https://developer-api.nest.com/devices/thermostats",
-      headers: {authorization: "Bearer #{credentials[:access_token]}",
-                content_type: 'application/json'})
+      url: 'https://developer-api.nest.com/devices/thermostats',
+      headers: { authorization: "Bearer #{credentials[:access_token]}",
+                 content_type: 'application/json' }
+    )
     thermostats = JSON.parse response
-    $logger.info thermostats
+    @logger.info thermostats
 
     influxdb = InfluxDB::Client.new 'nest'
 
-    thermostats.values.each { |ts|
-      $logger.debug ts
+    thermostats.values.each do |ts|
+      @logger.debug ts
       # ambient_temperature_f => 73
       # hvac_state => "heating", "cooling", "off"
       # last_connection => "2018-10-15T03:53:54.097Z"
 
-      timestamp = DateTime.parse(ts['last_connection']).to_time.to_i
+      timestamp = Time.parse(ts['last_connection']).to_i
 
-      if not ts['fan_timer_active'].nil?
+      unless ts['fan_timer_active'].nil?
         data = {
           values: { value: ts['fan_timer_active'] },
-          tags:   { name_long: ts['name_long'] },
+          tags: { name_long: ts['name_long'] },
           timestamp: timestamp
         }
         influxdb.write_point('fan_timer_active', data)
       end
 
-      if not ts['hvac_mode'].nil?
+      unless ts['hvac_mode'].nil?
         data = {
           values: { value: ts['hvac_mode'] },
-          tags:   { name_long: ts['name_long'] },
+          tags: { name_long: ts['name_long'] },
           timestamp: timestamp
         }
         influxdb.write_point('hvac_mode', data)
       end
 
-      if not ts['hvac_state'].nil?
+      unless ts['hvac_state'].nil?
         data = {
           values: { value: ts['hvac_state'] },
-          tags:   { name_long: ts['name_long'] },
+          tags: { name_long: ts['name_long'] },
           timestamp: timestamp
         }
         influxdb.write_point('hvac_state', data)
       end
 
-      if not ts['ambient_temperature_f'].nil?
+      unless ts['ambient_temperature_f'].nil?
         data = {
           values: { value: ts['ambient_temperature_f'].to_f },
-          tags:   { name_long: ts['name_long'] },
+          tags: { name_long: ts['name_long'] },
           timestamp: timestamp
         }
         influxdb.write_point('ambient_temperature_f', data)
       end
 
-      if not ts['humidity'].nil?
+      unless ts['humidity'].nil?
         data = {
           values: { value: ts['humidity'].to_i },
-          tags:   { name_long: ts['name_long'] },
+          tags: { name_long: ts['name_long'] },
           timestamp: timestamp
         }
         influxdb.write_point('humidity', data)
       end
-    }
+    end
   end
 end
 
