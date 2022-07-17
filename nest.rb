7bd180bbb34e53b8f45d2b7628848fa6d3584f43
Jeff Kowalski <jeff.kowalski@gmail.com>
2022-07-17 10:46:42 -0700

    modified to work with google home device access

--- nest.rb
+++ nest.rb
@@ -5,64 +5,68 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-class Nest < RecorderBotBase
-  desc 'authorize', '[re]authorize the application'
-  def authorize
-    credentials = load_credentials
-    state = Time.now.to_i
-    puts "Go to this URL https://home.nest.com/login/oauth2?client_id=#{credentials[:client_id]}&state=#{state}"
-    puts 'Copy PIN and paste it here:'
-    pin = $stdin.gets.chomp
-    response = RestClient.post('https://api.home.nest.com/oauth2/access_token',
-                               code: pin,
-                               client_id: credentials[:client_id],
-                               client_secret: credentials[:client_secret],
-                               grant_type: 'authorization_code')
-    token = JSON.parse(response)
-    credentials[:access_token] = token['access_token']
-    store_credentials credentials
+class Float
+  def c_to_f
+    9.0 * self / 5.0 + 32.0
   end
+end
+
+class Nest < RecorderBotBase
+  BASE_NEST_URL = 'https://smartdevicemanagement.googleapis.com/v1/'
 
   no_commands do
-    def main
+    def refresh_access_token
       credentials = load_credentials
 
-      response = with_rescue([RestClient::Exceptions::OpenTimeout], @logger) do |_try|
-        RestClient::Request.execute(
-          method: 'get',
-          url: 'https://developer-api.nest.com/devices/thermostats',
-          headers: { authorization: "Bearer #{credentials[:access_token]}",
-                     content_type: 'application/json' }
-        )
-      end
-      thermostats = JSON.parse response
-      @logger.info thermostats
+      response = RestClient.post('https://oauth2.googleapis.com/token',
+                                 client_id:     credentials[:client_id],
+                                 client_secret: credentials[:client_secret],
+                                 refresh_token: credentials[:refresh_token],
+                                 grant_type:    'refresh_token')
+      token = JSON.parse(response)
+      credentials[:access_token] = token['access_token']
+      store_credentials credentials
+    end
+
+    def main
+      refresh_access_token
+      credentials = load_credentials
 
       influxdb = InfluxDB::Client.new 'nest' unless options[:dry_run]
-
       data = []
-      thermostats.each_value do |ts|
-        @logger.debug ts
-        # ambient_temperature_f => 73
-        # hvac_state => "heating", "cooling", "off"
-        # last_connection => "2018-10-15T03:53:54.097Z"
 
-        timestamp = Time.parse(ts['last_connection']).to_i
+      url = BASE_NEST_URL + "enterprises/#{credentials[:project_id]}/devices"
+      response = RestClient.get(url, content_type: 'application/json', authorization: "Bearer #{credentials[:access_token]}")
+      devices = JSON.parse(response)['devices']
+      timestamp = DateTime.parse(response.headers[:date]).to_time.to_i
 
-        { 'fan_timer_active'      => ->(v) { v },
-          'hvac_mode'             => ->(v) { v },
-          'hvac_state'            => ->(v) { v },
-          'ambient_temperature_f' => ->(v) { v.to_f },
-          'target_temperature_f'  => ->(v) { v.to_f },
-          'humidity'              => ->(v) { v.to_i } }.each do |measure, transform|
-          next if ts[measure].nil?
+      devices.select { |dev| dev['type'] == 'sdm.devices.types.THERMOSTAT' }.each do |ts|
+        @logger.debug ts.pretty_inspect
 
-          data.push({ series: measure,
-                      values: { value: transform.call(ts[measure]) },
-                      tags: { name_long: ts['name_long'] },
+        traits = ts['traits']
+        name_long = traits['sdm.devices.traits.Info']['customName']
+        name_long = ts['parentRelations'][0]['displayName'] if name_long.empty?
+        measures = {
+          fan_timer_active:      traits['sdm.devices.traits.Fan']['timerMode']&.downcase == 'on',
+          hvac_mode:             traits['sdm.devices.traits.ThermostatEco']['mode'] == 'MANUAL_ECO' ? 'eco' : traits['sdm.devices.traits.ThermostatMode']['mode']&.downcase,
+          hvac_state:            traits['sdm.devices.traits.ThermostatHvac']['status']&.downcase,
+          ambient_temperature_f: traits['sdm.devices.traits.Temperature']['ambientTemperatureCelsius'].to_f.c_to_f,
+          humidity:              traits['sdm.devices.traits.Humidity']['ambientHumidityPercent'].to_i
+        }
+        if measures[:hvac_mode].include? 'heat'
+          measures[:target_temperature_f] = traits['sdm.devices.traits.ThermostatTemperatureSetpoint']['heatCelsius'].to_f.c_to_f
+        elsif measures[:hvac_mode].include? 'cool'
+          measures[:target_temperature_f] = traits['sdm.devices.traits.ThermostatTemperatureSetpoint']['coolCelsius'].to_f.c_to_f
+        end
+        measures.each do |measure, value|
+          data.push({ series: measure.to_s,
+                      values: { value: value },
+                      tags: { name_long: name_long },
                       timestamp: timestamp })
         end
       end
+
+      @logger.debug data.pretty_inspect
       influxdb.write_points data unless options[:dry_run]
     end
   end
